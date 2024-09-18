fe77889c61ef6e6ad3a066a4f4e3e898e5e9c0e8
Adrian Salas <ing.s.adrian@gmail.com>
2024-09-18 00:58:21 -0500

    first commit

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,9 @@ import {
 } from '@bot-whatsapp/bot'
 import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys'
 
-const flowWelcome = addKeyword('hola').addAnswer('Bienvenido!!')
+const flowWelcome = addKeyword('hola').addAnswer(
+  'https://55jlqmvv-4200.usw3.devtunnels.ms/1'
+)
 
 const main = async () => {
   const provider = createProvider(BaileysProvider)
@@ -18,8 +20,10 @@ const main = async () => {
     'send-message',
     handleCtx(async (bot, req, res) => {
       const { message, mediaUrl } = req.body
+      console.log('mensahe', message, mediaUrl)
+
       await bot.sendMessage('9981815054', message, {
-        media: mediaUrl
+        // media: mediaUrl
       })
       res.end('Mensaje desde el server')
     })
